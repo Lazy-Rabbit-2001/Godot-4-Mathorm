8519a9051baf759029dcc6c548375474ac914626
Lazy-Rabbit-2001 <2733679597@qq.com>
2024-01-04 21:14:13 +0800

    removed setup() from Ellipse

--- src/mathorm.h
+++ src/mathorm.h
@@ -7,7 +7,8 @@
 using namespace godot;
 
 // Calculus 
-class Calculus : public RefCounted {
+class Calculus : public RefCounted 
+{
     GDCLASS(Calculus, RefCounted)
 
 protected:
@@ -18,11 +19,9 @@ public:
         CALCULUS_SAMPLES = 2400,
     };
 
-    // Constructor and Destructor
     Calculus();
     ~Calculus();
 
-    // Methods
     static double get_integral_definite(const double from, const double to, const Callable &function, const int samples = CALCULUS_SAMPLES);
     static double get_derivative_at(const double x, const Callable &function, const double samples = 1.0 / double(CALCULUS_SAMPLES));
     static double get_legendre_elliptic_ii(double top, const double k, const int samples = CALCULUS_SAMPLES);    
@@ -30,7 +29,8 @@ public:
 
 
 // Transform2DAlgo
-class Transform2DAlgo : public RefCounted {
+class Transform2DAlgo : public RefCounted 
+{
     GDCLASS(Transform2DAlgo, RefCounted)
 
 protected:
@@ -44,7 +44,8 @@ public:
 
 // Geomentry
 // Ellipse
-class Ellipse : public RefCounted {
+class Ellipse : public RefCounted 
+{
     GDCLASS(Ellipse, RefCounted)
 
 private:
@@ -56,12 +57,8 @@ protected:
     static void _bind_methods();
 
 public:
-    // Constructor and Destructor
     Ellipse(const Vector2 center = Vector2(0, 0), const Vector2 amplitude = Vector2(1, 1), const double rotation = 0.0);
     ~Ellipse();
-
-    // Methods
-    Ellipse* Ellipse::setup(const Vector2 &center, const Vector2 &amplitude, double rotation);
 
     double get_long_axis() const;
     double get_short_axis() const;
@@ -70,16 +67,12 @@ public:
     double get_ellipse_area() const;
     double get_circumference() const;
     double get_circumference_accurate(const int samples = 2400) const;
-
     Vector2 get_point(const double phase) const;
 
-    // Properties' Setters & Getters
     void set_center(const Vector2 &p_center);
     Vector2 get_center() const;
-
     void set_amplitude(const Vector2 &p_amplitude);
     Vector2 get_amplitude() const;
-
     void set_rotation(const double p_rotation);
     double get_rotation() const;
 };
--- src/mathrom.cpp
+++ src/mathrom.cpp
@@ -7,22 +7,21 @@ using namespace godot;
 
 
 // Calculus
-void Calculus::_bind_methods() {
-    // Register constants
+void Calculus::_bind_methods()
+{
     BIND_CONSTANT(CALCULUS_SAMPLES);
-    // Register methods
+
     ClassDB::bind_static_method("Calculus", D_METHOD("get_integral_definite", "from", "to", "function", "samples"), &Calculus::get_integral_definite, CALCULUS_SAMPLES);
     ClassDB::bind_static_method("Calculus", D_METHOD("get_derivative_at", "x", "function", "samples"), &Calculus::get_derivative_at, 1.0 / double(CALCULUS_SAMPLES));
     ClassDB::bind_static_method("Calculus", D_METHOD("get_legendre_elliptic_ii", "top", "k", "samples"), &Calculus::get_legendre_elliptic_ii, CALCULUS_SAMPLES);
 }
 
-// Constructor and Destructor
 Calculus::Calculus() {}
 
 Calculus::~Calculus() {}
 
-// Methods
-double Calculus::get_integral_definite(const double from, const double to, const Callable &function, const int samples) {
+double Calculus::get_integral_definite(const double from, const double to, const Callable &function, const int samples)
+{
     if (samples < CALCULUS_SAMPLES) {
         UtilityFunctions::printerr("Sample amount ", samples, " is lower than ", CALCULUS_SAMPLES, ", integral will become more inaccurate!");
     }
@@ -46,7 +45,8 @@ double Calculus::get_integral_definite(const double from, const double to, const
     return ret;
 }
 
-double Calculus::get_derivative_at(const double x, const Callable &function, const double samples) {
+double Calculus::get_derivative_at(const double x, const Callable &function, const double samples)
+{
     if (samples > CALCULUS_SAMPLES) {
         UtilityFunctions::printerr("Sample amount ", samples, " is greater than ", 1.0 / CALCULUS_SAMPLES, ", derivative will become more inaccurate!");
     }
@@ -54,7 +54,8 @@ double Calculus::get_derivative_at(const double x, const Callable &function, con
     return (double(function.call(x + samples)) - double(function.call(x))) / samples;
 }
 
-double Calculus::get_legendre_elliptic_ii(const double top, const double k, const int samples) {
+double Calculus::get_legendre_elliptic_ii(const double top, const double k, const int samples)
+{
     auto lambda = [](double x, double s) {
         return sqrt(1 - s * pow(sin(x), 2.0));
     };
@@ -65,15 +66,17 @@ double Calculus::get_legendre_elliptic_ii(const double top, const double k, cons
     return get_integral_definite(0, top, func, samples);
 }
 
-    
+
+
 // Transform2DAlgo
-void Transform2DAlgo::_bind_methods() {
-    // Register methods
+void Transform2DAlgo::_bind_methods()
+{
     ClassDB::bind_static_method("Transform2DAlgo", D_METHOD("get_projection_limit", "vector", "onto", "length"), &Transform2DAlgo::get_projection_limit);
     ClassDB::bind_static_method("Transform2DAlgo", D_METHOD("get_direction_to_regardless_transform", "origin", "target", "trans", "index"), &Transform2DAlgo::get_direction_to_regardless_transform, 0);
 }
 
-Vector2 Transform2DAlgo::get_projection_limit(const Vector2 &vector, const Vector2 &onto, const double length) {
+Vector2 Transform2DAlgo::get_projection_limit(const Vector2 &vector, const Vector2 &onto, const double length)
+{
     double dot = vector.dot(onto);
     if (dot < 0 || UtilityFunctions::is_zero_approx(UtilityFunctions::snappedf(dot, 0.001))) {
         return vector;
@@ -85,7 +88,8 @@ Vector2 Transform2DAlgo::get_projection_limit(const Vector2 &vector, const Vecto
     return double(v.length_squared()) > pow(length, 2.0) ? vector - d : vector;
 }
 
-int Transform2DAlgo::get_direction_to_regardless_transform(const Vector2 &origin, const Vector2 &target, const Transform2D &trans, int index) {
+int Transform2DAlgo::get_direction_to_regardless_transform(const Vector2 &origin, const Vector2 &target, const Transform2D &trans, int index)
+{
     Transform2D atr = trans.affine_inverse();
     
     index = UtilityFunctions::clampi(index, 0, 1);
@@ -97,34 +101,21 @@ int Transform2DAlgo::get_direction_to_regardless_transform(const Vector2 &origin
 }
 
 
+
 // Geomentry
 // Ellipse
-void Ellipse::_bind_methods() {
-    // Register properties
-    // Vector2 center
+void Ellipse::_bind_methods()
+{
     ClassDB::bind_method(D_METHOD("get_center"), &Ellipse::get_center);
     ClassDB::bind_method(D_METHOD("set_center", "p_center"), &Ellipse::set_center);
-    ClassDB::add_property(
-        "Ellipse", PropertyInfo(Variant::VECTOR2, "center"),
-        "set_center", "get_center"
-    );
-    // Vector2 amplitude
+    ClassDB::add_property("Ellipse", PropertyInfo(Variant::VECTOR2, "center"), "set_center", "get_center");
     ClassDB::bind_method(D_METHOD("get_amplitude"), &Ellipse::get_amplitude);
     ClassDB::bind_method(D_METHOD("set_amplitude", "p_amplitude"), &Ellipse::set_amplitude);
-    ClassDB::add_property(
-        "Ellipse", PropertyInfo(Variant::VECTOR2, "amplitude"),
-        "set_amplitude", "get_amplitude"
-    );
-    // double rotation
+    ClassDB::add_property("Ellipse", PropertyInfo(Variant::VECTOR2, "amplitude"), "set_amplitude", "get_amplitude");
     ClassDB::bind_method(D_METHOD("get_rotation"), &Ellipse::get_rotation);
     ClassDB::bind_method(D_METHOD("set_rotation", "p_rotation"), &Ellipse::set_rotation);
-    ClassDB::add_property(
-        "Ellipse", PropertyInfo(Variant::VECTOR2, "rotation"),
-        "set_rotation", "get_rotation"
-    );
-
-    // Register methods
-    ClassDB::bind_method(D_METHOD("setup", "center", "amplitude", "rotation"), &Ellipse::setup, Vector2(), Vector2(1, 1), 0.0);
+    ClassDB::add_property("Ellipse", PropertyInfo(Variant::VECTOR2, "rotation"), "set_rotation", "get_rotation");
+    
     ClassDB::bind_method(D_METHOD("get_long_axis"), &Ellipse::get_long_axis);
     ClassDB::bind_method(D_METHOD("get_short_axis"), &Ellipse::get_short_axis);
     ClassDB::bind_method(D_METHOD("get_half_focal_distance"), &Ellipse::get_half_focal_distance);
@@ -134,8 +125,8 @@ void Ellipse::_bind_methods() {
     ClassDB::bind_method(D_METHOD("get_circumference_accurate", "samples"), &Ellipse::get_circumference_accurate, Calculus::CALCULUS_SAMPLES);
 }
 
-// Constructor and Destructor
-Ellipse::Ellipse(Vector2 center, Vector2 amplitude, double rotation) {
+Ellipse::Ellipse(Vector2 center, Vector2 amplitude, double rotation)
+{
     this->center = center;
     this->amplitude = amplitude;
     this->rotation = rotation;
@@ -143,39 +134,36 @@ Ellipse::Ellipse(Vector2 center, Vector2 amplitude, double rotation) {
 
 Ellipse::~Ellipse() {}
 
-Ellipse* Ellipse::setup(const Vector2 &center, const Vector2 &amplitude, const double rotation) {
-    this->center = center;
-    this->amplitude = amplitude;
-    this->rotation = rotation;
-    
-    return this;
-}
-
-// Methods
-double Ellipse::get_long_axis() const {
+double Ellipse::get_long_axis() const
+{
     return amplitude[amplitude.max_axis_index()];
 }
 
-double Ellipse::get_short_axis() const {
+double Ellipse::get_short_axis() const
+{
     return amplitude[amplitude.min_axis_index()];
 }
 
-double Ellipse::get_half_focal_distance() const {
+double Ellipse::get_half_focal_distance() const
+{
     double as = pow(get_long_axis(), 2.0);
     double bs = pow(get_short_axis(), 2.0);
 
     return sqrt(as - bs);
 }
 
-double Ellipse::get_eccentricity() const {
+double Ellipse::get_eccentricity() const
+{
     return get_half_focal_distance() / get_long_axis();
 }
 
-double Ellipse::get_ellipse_area() const {
+double Ellipse::get_ellipse_area() const
+{
     return Math_PI * get_long_axis() * get_short_axis();
 }
 
-double Ellipse::get_circumference() const {
+double Ellipse::get_circumference() const
+{
     double a = get_long_axis();
     double b = get_short_axis();
 
@@ -190,7 +178,8 @@ double Ellipse::get_circumference() const {
     return Math_PI * (a + b) * q;
 }
 
-double Ellipse::get_circumference_accurate(const int samples) const {
+double Ellipse::get_circumference_accurate(const int samples) const
+{
     double a = get_long_axis();
     double b = get_short_axis();
     double e = get_eccentricity();
@@ -202,31 +191,38 @@ double Ellipse::get_circumference_accurate(const int samples) const {
     return 4 * a * Calculus::get_legendre_elliptic_ii(Math_PI / 2.0, pow(e, 2.0), samples);
 }
 
-Vector2 Ellipse::get_point(const double phase) const {
+Vector2 Ellipse::get_point(const double phase) const
+{
     return center + Vector2(amplitude.x * cos(phase), amplitude.y * sin(phase)).rotated(rotation);
 }
 
-// Properties' Setters & Getters
-void Ellipse::set_center(const Vector2 &p_center) {
+
+void Ellipse::set_center(const Vector2 &p_center)
+{
     center = p_center;
 }
 
-Vector2 Ellipse::get_center() const {
+Vector2 Ellipse::get_center() const
+{
     return center;
 }
 
-void Ellipse::set_amplitude(const Vector2 &p_amplitude) {
+void Ellipse::set_amplitude(const Vector2 &p_amplitude)
+{
     amplitude = p_amplitude;
 }
 
-Vector2 Ellipse::get_amplitude() const {
+Vector2 Ellipse::get_amplitude() const
+{
     return amplitude;
 }
 
-void Ellipse::set_rotation(const double p_rotation) {
+void Ellipse::set_rotation(const double p_rotation)
+{
     rotation = p_rotation;
 }
 
-double Ellipse::get_rotation() const {
+double Ellipse::get_rotation() const
+{
     return rotation;
 }
